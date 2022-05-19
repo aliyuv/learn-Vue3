34ea460f5483ab304b75306fab4adf0c17f10b92
aliyu <1324149139@qq.com>
2022-05-19 16:10:35 +0800

    学习watch

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@
   <h1>{{ count }}</h1>
   <h1>{{ double }}</h1>
   <button @click="increase">+1</button>
+
+  <h1>{{ greetings }}</h1>
+  <button @click="increase">+1</button>
+  <button @click="updateGreeting">update title</button>
 </template>
 
 <script lang="ts">
-import {computed, defineComponent, reactive, toRefs, onMounted, onUpdated, onRenderTriggered} from "vue";
+import {computed, defineComponent, reactive, toRefs, onMounted, onUpdated, onRenderTriggered, watch, ref} from "vue";
 
 interface DataProps {
   count: number;
@@ -16,7 +20,7 @@ interface DataProps {
 
 export default defineComponent({
   name: "App",
-  setup() {
+  setup: function () {
     onMounted(() => {
       console.log("mounted");
     });
@@ -26,6 +30,7 @@ export default defineComponent({
     onRenderTriggered((event) => {
       console.log(event);
     });
+
     const data: DataProps = reactive({
       count: 0,
       increase: () => {
@@ -33,9 +38,21 @@ export default defineComponent({
       },
       double: computed(() => data.count * 2)
     });
+    const greetings = ref("");
+    const updateGreeting = () => {
+      greetings.value += "Hello!";
+    };
+    // watch 多个值，返回的也是多个值的数组
+    watch([greetings, () => data.count], (newValue, oldValue) => {
+      console.log("old", oldValue);
+      console.log("new", newValue);
+      document.title = "updated" + greetings.value + data.count;
+    });
     const refData = toRefs(data);
     return {
-      ...refData
+      ...refData,
+      greetings,
+      updateGreeting
     };
   }
 });
